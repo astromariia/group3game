786012147c358704f2b8c9c7ec200e84ea234da2
caedenw <caeden.wilson@okstate.edu>
2025-09-30 13:07:27 -0500

    added teleport to room_2

--- roguelike-concept/door_open.gd
+++ roguelike-concept/door_open.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
-func _ready():
-	pass
+@export var target_scene: PackedScene   # assign this in Inspector
 
-func _input(event):
-	if event.is_action_pressed('ui_accept'):
-		print(get_overlapping_bodies().size())
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):  # Add elf to "player" group in editor
+		if target_scene:
+			get_tree().change_scene_to_packed(target_scene)
+
+			
