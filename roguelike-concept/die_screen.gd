eae777d4bde7b63c996247a47c490b7736eac76a
astromariia <mariia.shevchenko13@gmail.com>
2025-10-08 13:29:58 -0500

    retry button

--- roguelike-concept/die_screen.gd
+++ roguelike-concept/die_screen.gd
@@ -4,7 +4,7 @@ func _ready()-> void:
 	MusicController.bgm_play()
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Main Menu/MainMenu.tscn")
+	get_tree().change_scene_to_file("res://room_2.tscn")
 
 
 
