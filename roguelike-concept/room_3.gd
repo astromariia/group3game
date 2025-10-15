302c8412665a1ebe60b052518f4257fbc98cabff
alruswall <fluttershymert@gmail.com>
2025-10-14 19:42:48 -0500

    Revert "Merge branch 'Audrey' of https://github.com/astromariia/group3game into Audrey"
    This reverts commit 99943895f739ace2c624fecd40ed62137772b308, reversing
    changes made to b29f5182200e142425f7e9e15997e9d532176bf3.

--- roguelike-concept/room_3.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _ready()-> void:
-	MusicController.bgm_play()
-	
-	var elf = $elf
-	var slot = $elf/CanvasLayer/InventoryGui
-	slot.set_item(elf.get_current_weapon_name())
-	elf.connect("weapon_changed", Callable(slot, "set_item"))
